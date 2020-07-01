99e036c6768f87909e125ee2ac6ea4969d2ffb8e
6ALLIKA <skorik009@gmail.com>
2020-07-01 21:02:45 +0300

    test

--- src/main/java/concurency/ConccurencyMain.java
+++ src/main/java/concurency/ConccurencyMain.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
-public class Main {
+public class ConccurencyMain {
     private static final int NUMBER_OF_THREADS = 5;
 
     public static void main(String[] args) throws InterruptedException {
