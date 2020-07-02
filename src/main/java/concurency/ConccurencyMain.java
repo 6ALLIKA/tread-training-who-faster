4fd1c7c4bd63bfa5d5e139c9aaf3774b7ff086a8
6ALLIKA <skorik009@gmail.com>
2020-07-02 11:23:37 +0300

    refactor

--- src/main/java/concurency/ConccurencyMain.java
+++ src/main/java/concurency/ConccurencyMain.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Future;
 
 public class ConccurencyMain {
     private static final int NUMBER_OF_THREADS = 5;
+    private static final int PART_OF_LIST = 200_000;
 
     public static void main(String[] args) throws InterruptedException {
         List<Integer> list = new ArrayList<>();
@@ -19,16 +20,16 @@ public class ConccurencyMain {
             list.add(i);
         }
 
-        Callable<Long> callList1 = new CallableClass(0, 200_000, list);
-        Callable<Long> callList2 = new CallableClass(200_000, 400_000, list);
-        Callable<Long> callList3 = new CallableClass(400_000, 600_000, list);
-        Callable<Long> callList4 = new CallableClass(600_000, 800_000, list);
-        Callable<Long> callList5 = new CallableClass(800_000, 1_000_000, list);
+        List<Callable<Long>> callableList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            callableList.add(new CallableClass(PART_OF_LIST * i,
+                    PART_OF_LIST * (i +1), list));
+        }
         ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
         long startCallable = System.currentTimeMillis();
-        List<Future<Long>> futures = executorService.invokeAll(List.of(callList1, callList2,
-                callList3, callList4, callList5));
+        List<Future<Long>> futures = executorService.invokeAll(callableList);
         long endCallable = System.currentTimeMillis();
         executorService.shutdownNow();
 
@@ -37,11 +38,10 @@ public class ConccurencyMain {
                     try {
                         return f.get();
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        throw new RuntimeException(e);
                     } catch (ExecutionException e) {
-                        e.printStackTrace();
+                        throw new RuntimeException(e);
                     }
-                    return 0;
                 })
                 .sum();
 
